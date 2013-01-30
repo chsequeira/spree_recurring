0b58ed8bd37effcbe38cea339188174603355124
Michael Bianco <iloveitaly@gmail.com>
2013-01-30 17:05:12 -0500

    Rspec running

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,16 +1,3 @@
-# TODO this should be able to be merged into the class_eval below
-Spree::Product.instance_eval do
-  delegate_belongs_to :master, :subscribable if Spree::Variant.table_exists? && Spree::Variant.column_names.include?("subscribable")
-end
-
 Spree::Product.class_eval do
-  attr_accessible :subscribable
-
-  def subscribable?
-    master.subscribable?
-  end
+  delegate_belongs_to :master, :subscribable
 end
-
-#TODO: removed in spree 1.2.x
-# Spree::Variant.additional_fields += [ {:name => 'Subscribable', :only => [:product,:variant], :use => 'select', :value => lambda { |controller, field| [["False", false], ["True", true]]  } } ]
-
